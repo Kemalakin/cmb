03cb3212acb2475d3f30744557371e0794d9a32b
jlazear <jlazear@gmail.com>
2015-01-08 17:02:57 -0500

    Crummy formalism in this. Rewrite it.

---
+++ data/get_data.py
@@ -0,0 +1,17 @@
+!/bin/python
+
+"""
+get_data.py
+jlazear
+1/6/15
+
+One-line description
+
+Long description
+
+Example:
+
+<example code here>
+"""
+__version__ = 20150106
+__releasestatus__ = 'beta'
